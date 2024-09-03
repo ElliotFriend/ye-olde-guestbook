d49bfa3ce41fbf1a317b174759e16cb74c9005ca
Elliot Voris <elliot@stellar.org>
2024-09-03 15:04:19 -0700

    remove old console.log statement

--- src/routes/api/contract-id/[signer]/+server.ts
+++ src/routes/api/contract-id/[signer]/+server.ts
@@ -2,7 +2,6 @@ import { server } from '$lib/server/passkeyServer';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params }) => {
-    console.log('server', server)
     const contractId = await server.getContractId(params.signer!);
 
     return new Response(String(contractId));
